064f1b1cefb2373e1ef2300e55f2b001a489d74c
Benjamin Bajic <benjamin.bajic@pragmatio.com>
2019-05-21 10:43:58 +0200

    learning

--- src/main/java/BrowserTest.java
+++ src/main/java/BrowserTest.java
@@ -1,5 +1,7 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BrowserTest {
@@ -11,7 +13,12 @@ public class BrowserTest {
         WebDriver webDriver = new ChromeDriver();
 
 
-        webDriver.get("http://warmane.com");
+        webDriver.get("http://google.com");
+
+        WebElement textBox = webDriver.findElement(By.name("q"));
+        WebElement submitButton = webDriver.findElement(By.name("btnK"));
+        textBox.sendKeys("Automation step by step");
+        submitButton.submit();
         Thread.sleep(3000);
         webDriver.close();
     }
