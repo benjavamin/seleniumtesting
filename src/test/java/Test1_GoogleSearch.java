fce0d48fe37cbfc5da31533d80e5e78e4b5e83ce
Benjamin Bajic <bajic.benjamin98@gmail.com>
2019-05-21 18:09:44 +0200

    login test

--- src/test/java/Test1_GoogleSearch.java
+++ src/test/java/Test1_GoogleSearch.java
@@ -10,6 +10,7 @@ public class Test1_GoogleSearch {
     public static void main(String[] args) throws InterruptedException {
         WebDriverManager.chromedriver().setup();
         //googleSearch();
+        testLogin();
     }
 
     public static void googleSearch() throws InterruptedException {
@@ -18,11 +19,25 @@ public class Test1_GoogleSearch {
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://google.com"); //open google website
-        driver.findElement(By.xpath("//input[@name='q'][@type='text']")).sendKeys("Google this."); //find input field and enter text
+        driver.findElement(By.xpath("//input[@name='q'")).sendKeys("Google this."); //find input field and enter text
         driver.findElement(By.xpath("//input[@name='btnK'][@type='submit']")).submit(); //find google search button and click it
         driver.close(); //close chrome
 
         System.out.println("Test Completed Successfully");
+    }
+
+    public static void testLogin(){
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://thedemosite.co.uk/login.php");
+        driver.findElement(By.xpath("//input[@name='username']")).sendKeys("user1");
+        driver.findElement(By.xpath("//input[@name='password'][@type='password']")).sendKeys("user1");
+        driver.findElement(By.xpath("//input[@name='FormsButton2'][@type='button']")).click();
+
+        if (driver.findElement(By.xpath("//b[text()='**Successful Login**']")).isDisplayed()){
+            System.out.println("Login successful.");
+        }
+        driver.close();
     }
 
 }
