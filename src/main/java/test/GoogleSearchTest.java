796f5b066b0a81db016b8d4e28ae157550e13fe1
Benjamin Bajic <benjamin.bajic@pragmatio.com>
2019-05-22 16:34:54 +0200

    page object model

--- src/main/java/test/GoogleSearchTest.java
+++ src/main/java/test/GoogleSearchTest.java
@@ -1,32 +1,38 @@
+package test;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.sql.SQLOutput;
+import pages.GoogleSearchPage;
 
-public class Test1_GoogleSearch {
+import java.sql.DriverManager;
+
+public class GoogleSearchTest {
+
+    private static WebDriver driver = null;
 
     public static void main(String[] args) throws InterruptedException {
-        WebDriverManager.chromedriver().setup();
-        //googleSearch();
-        testLogin();
+        googleSearch();
+        //testLogin();
     }
 
     public static void googleSearch() throws InterruptedException {
-
-         //setup chromedriver for google chrome
-        WebDriver driver = new ChromeDriver();
-
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver(); //
         driver.get("https://google.com"); //open google website
-        driver.findElement(By.xpath("//input[@name='q'")).sendKeys("Google this."); //find input field and enter text
-        driver.findElement(By.xpath("//input[@name='btnK'][@type='submit']")).submit(); //find google search button and click it
+        //driver.findElement(By.xpath("//input[@name='q'")).sendKeys("Google this."); //find input field and enter text
+        //driver.findElement(By.xpath("//input[@name='btnK'][@type='submit']")).submit(); //find google search button and click it
+
+        GoogleSearchPage.textboxSearch(driver).sendKeys("Automation testing done right.");
+        GoogleSearchPage.buttonSearch(driver).submit();
         driver.close(); //close chrome
 
         System.out.println("Test Completed Successfully");
     }
 
-    public static void testLogin(){
+    public static void testLogin() {
         WebDriver driver = new ChromeDriver();
 
         driver.get("http://thedemosite.co.uk/login.php");
@@ -34,7 +40,7 @@ public class Test1_GoogleSearch {
         driver.findElement(By.xpath("//input[@name='password'][@type='password']")).sendKeys("user1");
         driver.findElement(By.xpath("//input[@name='FormsButton2'][@type='button']")).click();
 
-        if (driver.findElement(By.xpath("//b[text()='**Successful Login**']")).isDisplayed()){
+        if (driver.findElement(By.xpath("//b[text()='**Successful Login**']")).isDisplayed()) {
             System.out.println("Login successful.");
         }
         driver.close();
