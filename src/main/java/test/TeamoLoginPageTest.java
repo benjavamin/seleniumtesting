b070649c2f839729b1445dd119c3b2a7f3c85354
Benjamin Bajic <benjamin.bajic@pragmatio.com>
2019-05-24 14:50:14 +0200

    teamo login test using Test NG

--- src/main/java/test/TeamoLoginPageTest.java
+++ src/main/java/test/TeamoLoginPageTest.java
@@ -3,6 +3,9 @@ package test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 import pages.TeamoHomePage;
 import pages.TeamoLoginPage;
 
@@ -14,17 +17,16 @@ public class TeamoLoginPageTest {
     public static final String ANSI_GREEN = "\u001B[32m";
     public static final String ANSI_RED = "\u001B[31m";
 
-
-    public static void main(String[] args) {
-        teamoLoginTest();
-    }
-
-
-    public static void teamoLoginTest(){
+    @BeforeTest
+    public void setUpTest() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
+    }
+
+    @Test
+    public static void teamoLoginTest() {
         TeamoLoginPage teamoLoginPage = new TeamoLoginPage(driver);
-        TeamoHomePage teamoHomePage = new TeamoHomePage(driver);
+
 
         driver.get("http://localhost:4200/login");
         teamoLoginPage.enterUsername("benjaminbajic@mail.com");
@@ -32,8 +34,15 @@ public class TeamoLoginPageTest {
         teamoLoginPage.signIn();
         driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-        boolean pass = teamoHomePage.iconDisplayCheck();
 
+
+    }
+
+    @AfterTest
+    public void tearDownTest() {
+
+        TeamoHomePage teamoHomePage = new TeamoHomePage(driver);
+        boolean pass = teamoHomePage.iconDisplayCheck();
         if (pass) {
             System.out.println("Teamo Login Test: " + ANSI_GREEN + "PASS");
         } else {
