ffcc59950d23cb7a4d820c1da7437b9abd91ba07
Benjamin Bajic <benjamin.bajic@pragmatio.com>
2019-05-24 10:11:08 +0200

    teamo login test

--- src/main/java/pages/TeamoHomePage.java
+++ src/main/java/pages/TeamoHomePage.java
@@ -2,12 +2,11 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 
 public class TeamoHomePage {
     WebDriver driver = null;
     By iconSearch = By.xpath("//mat-icon[contains(text(),'account_circle')]");
-    By signOutBtnSearch = By.xpath("//span[contains(text(),'Sign out')]");
+
 
 
     public TeamoHomePage(WebDriver driver) {
@@ -15,11 +14,12 @@ public class TeamoHomePage {
     }
 
     public boolean iconDisplayCheck(){
-        return driver.findElement(iconSearch).isDisplayed();
+        try {
+            return driver.findElement(iconSearch).isDisplayed();
+        } catch (Exception ex) {
+            return false;
+        }
     }
 
-    public WebElement signOutBtn(){
-        WebElement signOutBtn = driver.findElement(signOutBtnSearch);
-        return signOutBtn;
-    }
+
 }
