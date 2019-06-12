920a6a9a41b63e9337d0978766f66924aa5e8974
Benjamin Bajic <benjamin.bajic@pragmatio.com>
2019-06-12 09:31:21 +0200

    BaseTest class added

--- src/main/java/test/BaseTest.java
+++ src/main/java/test/BaseTest.java
@@ -1,4 +1,30 @@
 package test;
 
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
 public class BaseTest {
+
+    private WebDriver driver;
+
+    @BeforeSuite
+    public void beforeSuite(){
+        driver = new ChromeDriver();
+        ChromeDriverManager.chromedriver().setup();
+    }
+
+    @AfterSuite
+    public void afterSuite(){
+        if (driver != null) {
+            driver.close();
+            driver.quit();
+        }
+    }
+
+    public WebDriver getDriver(){
+        return this.driver;
+    }
 }
